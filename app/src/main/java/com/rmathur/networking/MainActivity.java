03d6cb679efbc8606cb9801cfa509f7452ef65da
Rohan Mathur <rohan@rmathur.com>
2015-03-03 13:53:01 -0600

    Push autocompleting with networking on main thread

--- app/src/main/java/com/rmathur/networking/MainActivity.java
+++ app/src/main/java/com/rmathur/networking/MainActivity.java
@@ -1,17 +1,103 @@
 package com.rmathur.networking;
 
+import android.graphics.Color;
+import android.os.StrictMode;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    EditText edtSearchStops;
+    ListView lstStopList;
+    ArrayList<String> results = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtSearchStops = (EditText) findViewById(R.id.editText);
+        lstStopList = (ListView) findViewById(R.id.listView);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        edtSearchStops.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                try {
+                    results.clear();
+                    String userText = edtSearchStops.getText().toString();
+                    if (userText.equals("isr")) {
+                        userText = "Illinois Street Residence Hall";
+                    }
+                    userText = userText.replace(" ", "+").toLowerCase();
+                    String query = getStops(userText);
+
+                    JSONArray jsonArray = new JSONArray(query);
+                    HashMap<String, String> wipStopList = new HashMap<String, String>();
+
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        String stop_id = jsonArray.getJSONObject(i).getString("i");
+                        String name = jsonArray.getJSONObject(i).getString("n");
+                        wipStopList.put(name, stop_id);
+                        results.add(name);
+                    }
+
+                    if (!results.isEmpty() || results != null) {
+                        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, results) {
+                            @Override
+                            public View getView(int position, View convertView, ViewGroup parent) {
+                                View view = super.getView(position, convertView, parent);
+                                TextView textView = (TextView) view.findViewById(android.R.id.text1);
+                                textView.setTextColor(Color.BLACK);
+                                return view;
+                            }
+                        };
+                        lstStopList.setAdapter(arrayAdapter);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
 
@@ -36,4 +122,30 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public String getStops(String query) {
+        StringBuilder builder = new StringBuilder();
+        HttpClient client = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet(getString(R.string.autocompleteAPI) + query);
+        try {
+            HttpResponse response = client.execute(httpGet);
+            StatusLine statusLine = response.getStatusLine();
+            int statusCode = statusLine.getStatusCode();
+            if (statusCode == 200) {
+                HttpEntity entity = response.getEntity();
+                InputStream content = entity.getContent();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    builder.append(line);
+                }
+            } else {
+                Log.e(MainActivity.class.toString(), "Failed to get JSON object");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return builder.toString();
+    }
+
 }
